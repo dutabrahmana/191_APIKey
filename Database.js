1557d0f545f63eb327e3dce75066e922219f555f
dutabrahmana <dutalinggau25@gmail.com>
2025-11-11 21:03:48 +0700

    menambahkan validasi jikalau dtabse eror dan berhasil

--- Database.js
+++ Database.js
@@ -8,3 +8,12 @@ const db = mysql.createConnection({
   port: 3309
 });
 
+db.connect((err) => {
+  if (err) {
+    console.error('❌ Gagal konek ke database:', err);
+  } else {
+    console.log('✅ Terhubung ke database MySQL apikey_db!');
+  }
+});
+
+module.exports = db;
